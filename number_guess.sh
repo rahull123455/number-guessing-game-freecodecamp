8119d714cf8900d98d6f6e499cf82ca60e0d0330
Vinayak kumar <125748758+rahull123455@users.noreply.github.com>
2026-07-16 01:42:03 +0530

    feat: complete number guessing game

---
+++ number_guess.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PSQL="psql --username=postgres --dbname=number_guess -t --no-align -c"
+
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
+NUMBER_OF_GUESSES=0
+
+echo "Enter your username:"
+read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
+
+if [[ -z $USER_ID ]]
+then
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID;")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID;")
+
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+echo "Guess the secret number between 1 and 1000:"
+
+while true
+do
+  read GUESS
+  ((NUMBER_OF_GUESSES++))
+
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  elif (( GUESS > SECRET_NUMBER ))
+  then
+    echo "It's lower than that, guess again:"
+  elif (( GUESS < SECRET_NUMBER ))
+  then
+    echo "It's higher than that, guess again:"
+  else
+    break
+  fi
+done
+
+if [[ -z $USER_ID ]]
+then
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
+fi
+
+INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES);")
+
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
